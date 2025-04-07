3d578e80d12a539b573cafcfd0a7b011f86ffb66
Josavina1 <josavina.abbey@acity.edu.gh>
2025-04-07 14:24:57 +0000

    updated cors

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors('*'));
-app.use(express.json());
+const allowedOrigins = ['https://josavina1.github.io'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));app.use(express.json());
 
 let items = [];
 let idCounter = 1;
@@ -16,14 +25,14 @@ app.post('/items', (req, res) => {
     const { text } = req.body;
     const newItem = { id: idCounter++, text };
     items.push(newItem);
-    console.log("New Item added")
+    console.log("New Item Added")
     res.status(201).json(newItem);
 });
 
 app.delete('/items/:id', (req, res) => {
     const id = parseInt(req.params.id);
     items = items.filter(item => item.id !== id);
-    console.log("item Deleted")
+    console.log("Item Deleted")
     res.sendStatus(204);
 });
 
